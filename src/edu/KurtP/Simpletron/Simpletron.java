3d187e0c174cdd27d89afe026e26bc8835ff7ac5
Kurt P <krprudhomme@gmail.com>
2012-11-29 14:28:03 -0600

    Got input request working
    The Simpletron now asks for input and can give output with instrustions
    1000 and 1100

--- src/edu/KurtP/Simpletron/Simpletron.java
+++ src/edu/KurtP/Simpletron/Simpletron.java
@@ -1,44 +1,94 @@
 /*
- * This is my first draft for the Simpletron program that I need to do for a 
+ * This is my first draft for the Simpletron program that I need to do for a
  * class project.
  */
 package edu.KurtP.Simpletron;
 
+import java.util.Scanner;
+import edu.KurtP.Simpletron.SimpletronOperationCodes;
+import edu.KurtP.Simpletron.SimpletronAccumlator;
+import edu.KurtP.Simpletron.SimpletronInstructionRegulatiors;
+
 /**
  * @author kurtprudhomme
- * @version 0.0.11282012
+ * @version 0.1.11282012
  */
 public class Simpletron {
+
     private int[] memory = new int[100];
-    
-    //Read and write operations
-    private final int READ = 10;
-    private final int WRITE = 11;
-    
-    //Load and store operations
-    private final int LOAD = 20;
-    private final int STORE = 21;
-    
-    //Arithemetic operations
-    private final int ADD = 30;
-    private final int SUBTRACT = 31;
-    private final int DIVIDE = 32;
-    private final int MULITPLY = 33;
-    
-    //acculaltor should be a class and main memroy should be a class
-    
-    //Transfer of controle operations
-    private final int BRANCH = 40;
-    private final int BRANCHING = 41;
-    private final int BRANCHZERO = 42;
-    private final int HALT = 43;
-    
+    private String string = "";
+
+    private int instructionCounter, instructionRegister = 0;
+
+    private int operationCode, operand;
+
     public void welcomeMessage() {
-        System.out.println("*** Welcome to Simpletron! ***");
+        System.out.println("***            Welcome to Simpletron!           ***");
         System.out.println("*** Please enter your program one instruction   ***");
         System.out.println("*** (or data word) at a time. I will display    ***");
         System.out.println("*** the location number and a question mark (?) ***");
         System.out.println("*** You then type the word for that location.   ***");
         System.out.println("*** Type -99999 to stop entering your program.  ***");
+    }
+
+    public void runSimpletron() {
+        Scanner codeInputter = new Scanner(System.in);
+        int instructionInput = 0;
+        int memoryPointer = 0;
+
+//        do {
+//            System.out.printf("%02d ? ", SimpletronInstructionRegulatiors.getInstructionCounter());
+//            instructionInput = scanner.nextInt();
+//            memory[SimpletronInstructionRegulatiors.getInstructionCounter()] = instructionInput;
+//            SimpletronInstructionRegulatiors.setInstructionRegister(memory[SimpletronInstructionRegulatiors.getInstructionCounter()]);
+//            SimpletronInstructionRegulatiors.incrementInstructionCounter();
+//        }
+//        while (instructionInput != -99999);
+
+        do {
+            //Output the code input prompt
+            System.out.printf("%02d ? ", SimpletronInstructionRegulatiors.getInstructionCounter());
+            //Take the user input and assign it to the input var.
+            instructionInput = codeInputter.nextInt();
+
+            //place the input into the correct memory location
+            memory[memoryPointer] = instructionInput;
+            memoryPointer++;
+        }
+        while (instructionInput != -99999);
+
+        for (int code : memory) {
+            loadCode();
+            operations(operationCode, operand);
+        }
+    }
+
+    private void loadCode() {
+//        operationCode = memory[SimpletronInstructionRegulatiors.getInstructionCounter()] / 100;
+//        operand = memory[SimpletronInstructionRegulatiors.getInstructionCounter()] % 100;
+        operationCode = memory[instructionCounter] / 100;
+        operand = memory[instructionCounter] % 100;
+    }
+
+    private void operations(int operationCode, int operand) {
+
+        switch(operationCode) { //Start switch
+
+            //Operations for reading input from the user
+            case SimpletronOperationCodes.READ :
+                Scanner read = new Scanner(System.in);
+                System.out.print("Enter a number: ");
+                int number = read.nextInt();
+                memory[operand] = number;
+                break;
+
+            //Operations for outputting to the user
+            case SimpletronOperationCodes.WRITE :
+                System.out.println(memory[operand]);
+                break;
+        } //End switch
+
+//        SimpletronInstructionRegulatiors.incrementInstructionCounter();
+        instructionCounter++;
     }
 }
